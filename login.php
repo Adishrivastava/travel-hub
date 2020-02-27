4cc0bb1417315ed8c2c4ea1dd66cb5e2eb0704c4
Adishrivastava <48150157+Adishrivastava@users.noreply.github.com>
2020-02-27 17:13:28 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,8 @@
- <?php
+<?php session_start();
    ini_set('display_errors', 1);
    ini_set('display_startup_errors', 1);
    error_reporting(E_ALL);
    include_once('includes/connection.php');
-   session_start();
-   
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
 
